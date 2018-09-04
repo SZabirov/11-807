59c6645b3fb845a9a0236879443e82139c9c2cc9
Salavat Zabirov <zab.sal42@gmail.com>
2018-09-04 15:38:13 +0300

    Lesson01: main, if

---
+++ Lesson01/Name.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Name {
+	public static void main(String[] args) {
+		System.out.println("Enter your name:");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("Hello, " + name);
+
+		int x = 5;
+		String s = "num";
+		String sx = s + x;
+		System.out.println(sx);
+	}
+}
+
+
+
+
+
+
+
+
+
