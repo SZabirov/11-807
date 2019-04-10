e4ebdcbe0cbc58c7b8fc0f37b590fdbb9a38fca7
Salavat Zabirov <zab.sal42@gmail.com>
2019-04-10 12:11:17 +0300

    Lesson36: synchronized + locks

--- Lesson36/src/com/company/CounterThread.java
+++ Lesson36/src/com/company/CounterThread.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CounterThread extends Thread {
     private int start;
     private int end;
@@ -11,10 +14,17 @@ public class CounterThread extends Thread {
         this.arr = arr;
     }
 
+    private static final Object MUTEX = new Object();
+    private static Lock lock = new ReentrantLock();
+
     @Override
     public void run() {
         for (int i = start; i < end; i++) {
-            Main.mainSum += arr[i];
+//            synchronized (MUTEX) {
+            lock.lock();
+            Main.mainSum = Main.mainSum + arr[i];
+            lock.unlock();
+//            }
         }
     }
 }
